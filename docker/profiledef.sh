c8a5b1d2f81357103ed7c0f832a5f6a0c8e7e8b3
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-12-25 21:40:37 +0300

    Create profiledef.sh

---
+++ docker/profiledef.sh
@@ -0,0 +1,21 @@
+iso_name='archiso'
+iso_label='NAME'
+iso_publisher='Arch Linux <https://archlinux.org>'
+iso_application='Arch Linux baseline'
+iso_version='v'
+install_dir='arch'
+buildmodes=('iso')
+bootmodes=('uefi-x64.systemd-boot.esp')
+arch='x86_64'
+pacman_conf="pacman.conf"
+airootfs_image_type='erofs'
+airootfs_image_tool_options=(
+  '-zlzma,level=109,dictsize=8388608'
+  '-Efragments,dedupe,force-inode-compact,ztailpacking'
+  '-C262144'
+  '--workers=0'
+)
+bootstrap_tarball_compression=('zstd' '-T0' '--ultra' '-20zc')
+file_permissions=(
+  ['/etc/shadow']='0:0:400'
+)
