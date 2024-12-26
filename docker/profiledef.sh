49cd107928a5547ccd4b0cd3745e30dd4a6b7866
nedorazrab0 <141725500+nedorazrab0@users.noreply.github.com>
2024-12-26 21:48:38 +0300

    Update profiledef.sh

--- docker/profiledef.sh
+++ docker/profiledef.sh
@@ -8,15 +8,14 @@ buildmodes=('iso')
 bootmodes=('uefi-x64.systemd-boot.esp')
 arch='x86_64'
 pacman_conf="pacman.conf"
-airootfs_image_type='squashfs'
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_type='erofs'
 #-zlzma,level=109,dictsize=8388608
-#airootfs_image_tool_options=(
-#  '-zlz4hc,level=12'
-#  #'-Efragments,dedupe,force-inode-compact,ztailpacking'
-#  '-C1048576'
-#  '--workers=0'
-#)
+airootfs_image_tool_options=(
+  '-zlz4hc,level=0'
+  '-Efragments,dedupe,force-inode-compact,ztailpacking'
+  '-C1048576'
+  '--workers=0'
+)
 bootstrap_tarball_compression=('zstd' '-T0' '--ultra' '-20zc')
 file_permissions=(
   ['/etc/shadow']='0:0:400'
